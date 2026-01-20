7d5ed61c82442613a003cdee72b3e4ef4abb2ce8
adil2133-git <adilaadi340@gmail.com>
2026-01-20 10:06:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// var userName = "adil";
+// var age = 20;
+// var isSubscribed = true;
+// var skills = ["html", "css", "js", "tailwindcss", "react", "redux"];
+// var emptyArray = [];
+// var userDetail = {
+//     name: "adil",
+//     age: 20,
+//     salary: 50000
+// };
---
+++ index.ts
@@ -0,0 +1,11 @@
+let userName: string = "adil";
+let age: number = 20;
+let isSubscribed: boolean = true;
+let skills: string[] = ["html","css","js","tailwindcss","react","redux"]
+let emptyArray: [] = [];
+let userDetail: {name: string; age: number; salary: number} = {
+    name: "adil",
+    age: 20,
+    salary: 50000
+}
+
